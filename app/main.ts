8cb1f9beb412e3151944a04d77440f30f5a44d92
Laurent Nullens <l.nullens@gmail.com>
2016-09-20 17:32:04 +0200

    Common module import for Async pipe .gitignore update

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,3 @@
-//import 'rxjs/Rx'; // load all features of reactive extensions
-
 import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform";
 import { AppModule } from "./app.module";
 
--- app/orderForm/orderforms.module.ts
+++ app/orderForm/orderforms.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from "@angular/common";
 
 import { OrderFormsRoutingModule, routedComponents } from './orderforms.routing';
 import { OrderFormService } from './service/orderForm-service';
 
 @NgModule({
-  imports: [OrderFormsRoutingModule],
+  imports: [OrderFormsRoutingModule, CommonModule],
   declarations: [routedComponents],
   providers: [OrderFormService]
 })
